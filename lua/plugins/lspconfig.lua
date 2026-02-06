3b87f852da9fca3e5c38b0ea2ed6d0a5d806b2f4
paulpeters144 <paul.peters144@gmail.com>
2026-02-06 09:25:38 -0600

    update

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -45,7 +45,7 @@ return {
             if vim.fn.has 'nvim-0.11' == 1 then
               return client:supports_method(method, bufnr)
             else
-              return client.supports_metkod(method, { bufnr = bufnr })
+              return client.supports_method(method, { bufnr = bufnr })
             end
           end
 
