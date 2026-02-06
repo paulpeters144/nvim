654029828afc24c55f0eb3a35aa242ea80edc9fc
paulpeters144 <paul.peters144@gmail.com>
2026-02-05 22:31:46 -0600

    update

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'echasnovski/mini.icons',
+    },
+    opts = {
+      markdown = {
+        block_quotes = { wrap = true },
+        headings = { org_indent_wrap = true },
+        list_items = { wrap = true },
+      },
+    },
+    config = function(_, opts)
+      require('markview').setup(opts)
+
+      -- Enable wrap for markdown files as requested
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'markdown',
+        callback = function()
+          vim.opt_local.wrap = true
+          vim.opt_local.linebreak = true
+        end,
+      })
+    end,
+  },
+}
