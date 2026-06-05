fd8322b45ec2150cb351d50e807cba62b6bb42e8
paulpeters144 <paul.peters144@gmail.com>
2026-06-05 15:33:53 -0500

    removed double arrow tabls

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -21,7 +21,7 @@ vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
 vim.o.inccommand = 'split'
 vim.o.cursorline = true
 vim.o.scrolloff = 10
