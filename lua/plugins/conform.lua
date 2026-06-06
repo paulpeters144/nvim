0f4c9b25c7691509022c4c4b47b412984682d0cb
paulpeters144 <paul.peters144@gmail.com>
2026-06-06 14:28:33 -0500

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -21,7 +21,7 @@ return {
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 2000,
             lsp_format = 'fallback',
           }
         end
